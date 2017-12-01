6dd1beb744cfc3f74859a0d6875939a0ff2348b7
olddonny <reedtrekks@gmail.com>
2017-12-01 14:57:48 -0600

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SigninComponent } from './views/signin/signin.component';
+import { UserComponent } from './views/user/user.component';
+import { MatchingComponent } from './views/matching/matching.component';
+import { SearchComponent } from './forms/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    UserComponent,
+    MatchingComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
