caf8b291c9b6b527eaee88ecd5f2a8a83b14be7d
Erica-Brock <Eady.Brock@gmail.com>
2017-12-04 16:22:22 -0600

    added google map component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SigninComponent } from './views/signin/signin.component';
@@ -7,6 +8,8 @@ import { UserComponent } from './views/user/user.component';
 import { MatchingComponent } from './views/matching/matching.component';
 import { SearchComponent } from './forms/search/search.component';
 import { OktaService } from './services/okta.service';
+import { MapComponent } from './map/map.component';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
@@ -14,10 +17,19 @@ import { OktaService } from './services/okta.service';
     SigninComponent,
     UserComponent,
     MatchingComponent,
-    SearchComponent
+    SearchComponent,
+    MapComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey:'AIzaSyCD9OJOP9rkatl5IE0F42UjystaJYS-rBI',
+      libraries: [
+        'places'
+      ]
+    }),
   ],
   providers: [OktaService],
   bootstrap: [AppComponent]
